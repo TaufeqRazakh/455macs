d716d8bb63bce1118002cc46d1362da39a49fc6f
TaufeqRazakh <taufeqmdr@gmail.com>
2020-08-31 22:27:28 -0700

    removed fixed window size

--- init.el
+++ init.el
@@ -15,21 +15,5 @@
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 
-(add-to-list 'default-frame-alist '(height . 50))
-(add-to-list 'default-frame-alist '(width . 110))
-(setq initial-frame-alist '((left . 20) (top . 40)))
-
 (require 'j-package)
 (require 'j-java)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

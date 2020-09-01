b03fe75a68c032dc6d277d904815633cc862fd6a
TaufeqRazakh <taufeqmdr@gmail.com>
2020-08-31 22:22:22 -0700

    removed java utils

--- modules/j-java.el
+++ modules/j-java.el
@@ -2,13 +2,5 @@
 (use-package yasnippet :config (yas-global-mode))
 (use-package company)
 (use-package which-key :config (which-key-mode))
-(use-package meghanada :config (add-hook 'java-mode-hook
-          (lambda ()
-            ;; meghanada-mode on
-            (meghanada-mode t)
-            (flycheck-mode +1)
-            (setq c-basic-offset 2)
-            ;; use code format
-            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))))
 
 (provide 'j-java)

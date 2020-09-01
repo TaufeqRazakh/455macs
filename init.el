d48a7e6204d24e5ae7bc27ff5f631831b2800d9e
TaufeqRazakh <taufeqmdr@gmail.com>
2020-09-01 02:24:35 -0700

    added line numbers to java files

--- init.el
+++ init.el
@@ -20,16 +20,3 @@
 
 (require 'j-package)
 (require 'j-java)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(java-snippets auto-complete yasnippet flycheck use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

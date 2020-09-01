36a4a778397c949aa76b0e646b6b391660f29a2d
TaufeqRazakh <taufeqmdr@gmail.com>
2020-08-31 22:36:26 -0700

    added auto-complete

--- modules/j-java.el
+++ modules/j-java.el
@@ -1,6 +1,5 @@
 (use-package flycheck)
 (use-package yasnippet :config (yas-global-mode))
-(use-package company)
-(use-package which-key :config (which-key-mode))
+(use-package auto-complete :config (ac-config-default))
 
 (provide 'j-java)

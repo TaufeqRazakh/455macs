fadc7bcd9b9d381ed92c4758997910a20a9db8e7
TaufeqRazakh <taufeqmdr@gmail.com>
2020-08-31 23:09:20 -0700

    added pair mode

--- modules/j-java.el
+++ modules/j-java.el
@@ -1,6 +1,6 @@
-(use-package flycheck)
-(use-package yasnippet :config (yas-global-mode))
 (use-package auto-complete :config (ac-config-default))
-(use-package java-snippets)
+(use-package yasnippet :config (yas-global-mode))
+
+(electric-pair-mode)
 
 (provide 'j-java)
